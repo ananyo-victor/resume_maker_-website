4d423449e11e10421c4aff1de1e86338a1e9ef01
ananyo-victor <ananyomaitroan@gmail.com>
2022-10-22 18:08:50 +0530

    updated on 22-10

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // for page 2
 
-let i = 1;
+const i = 1;
 function addNewWEField() {
 
-    let t = ++i;
+    const t = ++i;
     function sjob() {
         return "sJobDiv" + t;
     }
@@ -35,7 +35,7 @@ function addNewWEField() {
         return "EndSpan" + t;
     }
     function closespan() {
-        return "EndSpan" + (t-1);
+        return "EndSpan" + (t - 1);
     }
     function lempjobdiv() {
         return "LempJobDiv" + t;
@@ -368,3 +368,67 @@ function addNewWEField() {
     // console.log(closespan());
 }
 
+
+// page 5
+
+function OpenScreen2() {
+    const SmallScreen_2OB = document.getElementById('SmallScreen_2');
+    SmallScreen_2OB.removeAttribute('class', 'hide');
+    SmallScreen_2OB.classList.add('flex');
+    SmallScreen_2OB.classList.add('w-[600px]');
+    SmallScreen_2OB.classList.add('flex-col');
+    SmallScreen_2OB.classList.add('bg-yellow-300');
+    SmallScreen_2OB.classList.add('mx-auto');
+
+
+    const SmallScreen_1OB = document.getElementById('SmallScreen_1');
+    SmallScreen_1OB.classList.add('hide');
+
+}
+
+function OpenScreen1() {
+    const SmallScreen_1OB = document.getElementById('SmallScreen_1');
+    SmallScreen_1OB.removeAttribute('class', 'hide');
+    SmallScreen_1OB.classList.add('w-[600px]');
+    SmallScreen_1OB.classList.add('mx-auto');
+
+
+    const SmallScreen_2OB = document.getElementById('SmallScreen_2');
+    SmallScreen_2OB.classList.add('hidden');
+
+    const TopicNameOB = document.getElementById('TopicName');
+    TopicNameOB.classList.add('hide');
+}
+
+function add1(){
+    const add1OB = document.getElementById('BlankSpaceForStrength');
+    add1OB.value += 'Hardworking and passionate job seeker with strong organizational skills eager to secure entry-level Job Title position. Ready to help team achieve company goals.';
+}
+function add2(){
+    const add2OB = document.getElementById('BlankSpaceForStrength');
+    add2OB.value += 'Enthusiastic Job Title eager to contribute to team success through hard work, attention to detail and excellent organizational skills. Clear understanding of Task Job Titleand training in Skill. Motivated to learn, grow and excel in Industry.';
+}
+function add3(){
+    const add3OB = document.getElementById('BlankSpaceForStrength');
+    add3OB.value += 'Dedicated Industry professional with history of meeting company goals utilizing consistent and organized practices. Skilled in working under pressure and adapting to new situations and challenges to best enhance the organizational brand.';
+}
+function add4(){
+    const add4OB = document.getElementById('BlankSpaceForStrength');
+    add4OB.value += 'To seek and maintain full-time position that offers professional challenges utilizing interpersonal skills, excellent time management and problem-solving skills.';
+}
+function add5(){
+    const add5OB = document.getElementById('BlankSpaceForStrength');
+    add5OB.value += 'Experienced Job Title with over Number years of experience in Industry. Excellent reputation for resolving problems and improving customer satisfaction.';
+}
+function add6(){
+    const add6OB = document.getElementById('BlankSpaceForStrength');
+    add6OB.value += 'Organized and dependable candidate successful at managing multiple priorities with a positive attitude. Willingness to take on added responsibilities to meet team goals.';
+}
+function add7(){
+    const add7OB = document.getElementById('BlankSpaceForStrength');
+    add7OB.value += 'Reliable employee seeking Job Title position. Offering excellent communication and good judgment.';
+}
+function bold(){
+    const boldOB = document.getElementById('BlankSpaceForStrength');
+    // boldOB.
+}
