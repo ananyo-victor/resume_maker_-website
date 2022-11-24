01013c59b847ac397ef31fb8e75c47fe38aedd34
ananyo-victor <ananyomaitroan@gmail.com>
2022-11-24 23:18:42 +0530

    updated

--- script0.js
+++ script0.js
@@ -1,22 +1,15 @@
-function close1() {
-    const temppageOB = document.getElementById('temppage');
-    temppageOB.setAttribute('class','hidden');
-
+function preview1() {
+    const previewOB = document.getElementById('preview');
+    previewOB.classList.toggle('hidden');
+    const cancelpreviewOB = document.getElementById('cancelPreview');
+    cancelpreviewOB.classList.toggle('hidden');
     const oneandallOB = document.getElementById('oneandall');
-    oneandallOB.removeAttribute('class','blur-sm');
-    oneandallOB.removeAttribute('class','fixed');
-    console.log("hi");
+    oneandallOB.classList.toggle('fixed');
+    oneandallOB.classList.toggle('blur-sm');
+    // console.log("hi");
 }
 function open1(){
     
 const miniwindowOB = document.getElementById('miniWindow');
 miniwindowOB.classList.toggle('hidden');
-}
-
-function close2(){
-    
-const miniwindowOB = document.getElementById('miniWindow');
-miniwindowOB.classList.toggle('hidden');
-miniwindowOB.classList.add('hidden');
-
 }
